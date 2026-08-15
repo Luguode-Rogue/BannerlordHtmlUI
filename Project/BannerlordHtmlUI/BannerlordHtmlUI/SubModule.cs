120bfd5a40ea9a51c22a5e219f64b7fce71d487c
Luguode-Rogue <66106855+Luguode-Rogue@users.noreply.github.com>
2026-08-15 10:31:54 +0800

    feat: install bridge error model patch

--- Project/BannerlordHtmlUI/BannerlordHtmlUI/SubModule.cs
+++ Project/BannerlordHtmlUI/BannerlordHtmlUI/SubModule.cs
@@ -33,6 +33,7 @@ namespace BannerlordHtmlUI
                 HtmlUiI18nBindingPatch.Install(HtmlUiService.Host);
                 HtmlUiStateBootstrapPatch.Install(HtmlUiService.Host);
                 HtmlUiBindingSchedulerPatch.Install(HtmlUiService.Host);
+                HtmlUiErrorModelPatch.Install(HtmlUiService.Host);
                 HtmlUiNavigationRacePatch.Install(HtmlUiService.Host);
                 HtmlUiService.NotifyGameContext("application", true);
                 if (!HtmlUiService.Pages.Contains("framework"))
